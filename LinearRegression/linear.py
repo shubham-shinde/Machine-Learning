5bd633fde86ce0bf32e783991636f0bfa947098d
shubham-shinde <shindes0321@gmail.com>
2018-09-30 21:50:19 +0530

    updated multiple linear regression

--- LinearRegression/linear.py
+++ LinearRegression/linear.py
@@ -14,8 +14,11 @@ dataset = pd.read_csv('Salary_Data.csv')
 #index location = iloc
 #dataset is a 2d matrix
 #select all row in first column
+#iloc=index location
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:,1:].values
+
+#sklearn is a scikit learn library of py
 
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1/3, random_state=0)
